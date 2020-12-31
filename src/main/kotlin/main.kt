86152cfeb48f5fd246af641efd25939e3e60a68d
박기쁨 <ppeum@baggippeum-ui-MacBookPro.local>
2020-12-31 18:15:00 +0900

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+import java.util.Date as UtilDate
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    //java.util.Date.date = new java.util.Date();
+    //var date = Date() import에 as UtilDate를 붙이면 에러나서 변경해줘야 함
+    var date = UtilDate()
+}
